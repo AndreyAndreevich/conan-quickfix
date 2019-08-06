49e7db5fc5075e8d603ef4240386f462ef5edf6a
Vyacheslav Shviderskiy <vshviderskiy@ourgapps.com>
2019-08-06 19:35:13 +0300

    fix

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class QuickFixConan(ConanFile):
         git = tools.Git(folder=self.name)
         git.clone("https://github.com/quickfix/quickfix")
         #git.checkout("v%s" % self.version)
-        git.checkout("fb2e3cf8f9cdef6e66c546d590797e903052d76a")
+        git.checkout("a46708090444826c5f46a5dbf2ba4b069b413c58")
         os.rename(os.path.join(self.name, "CMakeLists.txt"),
                   os.path.join(self.name, "CMakeListsOriginal.txt"))
         fd = os.open(os.path.join(self.name, "CMakeLists.txt"),
@@ -46,11 +46,12 @@ class QuickFixConan(ConanFile):
         cmake.definitions['HAVE_SSL'] = self.options.ssl
         source_folder = self.name
         cmake.configure(source_folder=source_folder)
-        if os.path.isfile(source_folder + "/src/C++/Except.h"):
-            if not os.path.isdir(source_folder + "/include/quickfix"):
-                os.makedirs(source_folder + "/include/quickfix")
-            open(source_folder + "/include/quickfix/Except.h", "wb").write(
-                open(source_folder + "/src/C++/Except.h", "rb").read())
+        if self.settings.os == "Windows":
+            if os.path.isfile(source_folder + "/src/C++/Except.h"):
+                if not os.path.isdir(source_folder + "/include/quickfix"):
+                    os.makedirs(source_folder + "/include/quickfix")
+                open(source_folder + "/include/quickfix/Except.h", "wb").write(
+                    open(source_folder + "/src/C++/Except.h", "rb").read())
         cmake.build()
 
     def package(self):
